3dbd0c40dc7cbe8c9ef10d553a629651ac1e3ebd
Josh Meyers <jmeyers314@gmail.com>
2017-05-01 14:57:24 -0400

    Document selectDonutTask

--- python/lsst/donut/selectDonut.py
+++ python/lsst/donut/selectDonut.py
@@ -28,17 +28,15 @@ import lsst.pipe.base as pipeBase
 
 
 class SelectDonutConfig(pexConfig.Config):
-
+    """Config for SelectDonut"""
     r1cut = pexConfig.Field(
         dtype=float, default=50.0,
         doc="Rejection cut flux25/flux3 [default: 50.0]",
     )
-
     r2cut = pexConfig.Field(
         dtype=float, default=1.05,
         doc="Rejection cut flux35/flux25 [default: 1.05]",
     )
-
     snthresh = pexConfig.Field(
         dtype=float, default=250.0,
         doc="Donut signal-to-noise threshold [default: 250.0]",
@@ -46,7 +44,35 @@ class SelectDonutConfig(pexConfig.Config):
 
 
 class SelectDonutTask(pipeBase.Task):
+    """!Select detected objects for donut fitting.
 
+    @anchor SelectDonutTask_
+
+    @section donut_selectDonut_Purpose  Description
+
+    Select detected objects for donut fitting.
+
+    Good candidates are those that likely correspond to stars and not galaxies,
+    are not blended, and have sufficient signal-to-noise ratio.  DM-8644 looked
+    into these issues.  The criteria that emerged there and are implemented
+    here are:
+
+    - Use the ratio of medium-aperture flux (25 pixels) to small-aperture flux
+      (3 pixels) to identify the "hole" in stellar donut images and reject
+      galactic donut images.  A good candidate has F25/F3 > ~50 or so.  This
+      threshold is `r1cut` in the class configuration.
+    - Use the ratio of large-aperture flux (35 pixels) to medium-aperture flux
+      to identify blends of donuts.  A good candidate has F35/F25 < 1.05 or so.
+      This threshold is `r2cut` in the class configuration.
+    - Use F25/F25err as a signal-to-noise ratio measure.  Good candidates have
+      F25/F25err >~ 250 or so.  This threshold is `snthresh` in the class
+      configuration.
+
+    @section donut_selectDonut_IO  Invoking the Task
+
+    This task is primarily designed to be run as a subtask of fitDonut.  The
+    main method is `run`, which takes a SourceCatalog and returns a subset.
+    """
     ConfigClass = SelectDonutConfig
     _DefaultName = "selectDonut"
 
@@ -58,6 +84,14 @@ class SelectDonutTask(pipeBase.Task):
     @pipeBase.timeMethod
     def run(self, icSrc):
         """!Select donuts
+
+        @param icSrc  A SourceCatalog of donut detections.  Must include the
+                      columns:
+                      base_CircularApertureFlux_35_0_flux
+                      base_CircularApertureFlux_25_0_flux
+                      base_CircularApertureFlux_3_0_flux
+                      base_CircularApertureFlux_25_0_fluxSigma
+        @returns      A subset of the input SourceCatalog.
         """
         s2n = (icSrc['base_CircularApertureFlux_25_0_flux'] /
                icSrc['base_CircularApertureFlux_25_0_fluxSigma'])
